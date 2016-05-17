4d56c215463cdeb5d1e731c936074e1415d4904a
Vincent <vincentschen@yahoo.com>
2016-05-17 19:10:01 +0200

    better structural organization: separated REPL and Delivery classes

--- repl.py
+++ repl.py
@@ -1,1 +1,22 @@
-rep
+import cmd 
+from delivery import *
+
+class REPL(cmd.Cmd):
+
+    delivery = Delivery()
+    prompt = "> "
+    
+    def preloop(self): 
+        print self.delivery.initial_prompt
+
+    
+    def default(self, line): 
+        if line == ":quit":
+            return True 
+        else: 
+            response = self.delivery.process_input(line)
+            print response 
+            
+if __name__ == '__main__':
+    repl = REPL()
+    repl.cmdloop() 
