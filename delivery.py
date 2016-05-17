b4a023ab6683fc90bb12f83edea8ea65142da79c
Vincent <vincentschen@yahoo.com>
2016-05-17 19:40:16 +0200

    decompose processed input into different functions for each state

--- delivery.py
+++ delivery.py
@@ -5,8 +5,8 @@ class Delivery:
         #placeholder values
         self.app_name = "APP"
         self.order_details = {
-            "name": "NAME",
-            "item": "ITEM",
+            "name": "Vincent",
+            "item": "Toothbrush",
             "address": "44 HELLO STREET",
             "phone": 123456889
         }
@@ -20,35 +20,50 @@ class Delivery:
             'Do you recognize this purchase?') % (self.order_details['name'], self.app_name)    
     
     def process_input(self, input): 
+        #TODO: handle error checking if wrong value 
+        
         input = input.lower()
         response = ""
         
         if self.state == "INITIAL":
-            if input == "yes":
-                response = ('Thank you! \n'
-                    'Please confirm the following delivery information:\n'
-                    '\tNAME: %s\n'
-                    '\tADDRESS: %s\n'
-                    '\tCONTACT: %s\n'
-                    '\tITEM: %s\n'
-                    )
-            elif input == "no":
-                response = 'Thank you for your notification. We will cancel the order.'
-                self.quitting = True
+            response = self.initial(input)
             
         elif self.state == "CONFIRMATION":
-            if input == "yes":
-                self.state = "PENDING"
-
-            elif input == "no":
-                self.state == "INITIAL_CORRECTION"
-                response = ('Please respond with the proper information in the following format:\n'
-                    '\"FIELD:new_value\"')
+            response = self.confirmation(input)
                     
         elif self.state == "INITIAL_CORRECTION":
             # TODO: parse initial correction field 
             response = "Thank you for the correction!"
+            
+        # elif self.state == "ORDER_PLACED":
+            
                 
         return response 
+        
+    def initial(self, input): 
+        if input == "yes":
+            response = ('Thank you! \n'
+                'Please confirm the following delivery information:\n'
+                '\tNAME: %s\n'
+                '\tADDRESS: %s\n'
+                '\tCONTACT: %s\n'
+                '\tITEM: %s'
+                ) % (self.order_details['name'], self.order_details['address'], self.order_details['phone'], self.order_details['item'])
+        elif input == "no":
+            response = 'Thank you for your notification. We will cancel the order.'
+            self.quitting = True
+        return response 
+            
+    def confirmation(self, input): 
+        if input == "yes":
+            self.state = "ORDER_PLACED"
+            response = 'Wonderful! Please look forward to messages in the next few days regarding the progress of your order.'
+
+        elif input == "no":
+            self.state == "INITIAL_CORRECTION"
+            response = ('Please respond with the proper information in the following format:\n'
+                '\"FIELD:new_value\"')
+        return response 
+
         
     
